4197196914478d245b25b69a8f2440d44095cc8b
pkunjam <punit17100@iiitnr.edu.in>
2020-06-03 22:26:14 +0530

    User Input

---
+++ C# Survival Guide/Assets/Scripts/UserInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour {
+
+    public Vector3 start;
+
+	// Use this for initialization
+	void Start () {
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("Holding F");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pressed space");
+        }
+
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            Debug.Log("E lifted up");
+        }
+    }
+}
