00cc8c8fed09653cafba422317a8e83718898c84
pkunjam <punit17100@iiitnr.edu.in>
2020-06-04 11:55:25 +0530

    User Input

---
+++ C# Survival Guide/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Use this for initialization
+    float speed = 5;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal,0,0) * speed * Time.deltaTime);
+        transform.Translate(new Vector3(0,vertical,0) * speed * Time.deltaTime);
+	}
+}
