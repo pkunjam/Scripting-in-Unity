07e6cab3f733323c934de7ad5916b9a8cf25bb16
pkunjam <punit17100@iiitnr.edu.in>
2020-06-10 20:28:18 +0530

    Quaternion Identity

---
+++ C# Survival Guide/Assets/Scripts/CubeQuaternion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeQuaternion : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(cube, Vector3.zero, Quaternion.Euler(0,40,0));
+        }
+	}
+}
