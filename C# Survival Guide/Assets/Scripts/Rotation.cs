b696c164d85922205b5430ebb7e9b511f1384676
pkunjam <punit17100@iiitnr.edu.in>
2020-06-12 16:19:51 +0530

    Quaternion Slerp

--- C# Survival Guide/Assets/Scripts/Rotation.cs
+++ C# Survival Guide/Assets/Scripts/Rotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookRotation : MonoBehaviour {
+public class Rotation : MonoBehaviour {
 
     public Transform _sphere;
 
@@ -24,6 +24,8 @@ public class LookRotation : MonoBehaviour {
         Vector3 directiontoFace = _sphere.position - transform.position;
         Debug.DrawRay(transform.position, _sphere.position, Color.red);
 
-        transform.rotation = Quaternion.LookRotation(directiontoFace);
+        Quaternion targetRotation = Quaternion.LookRotation(directiontoFace);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2);
 	}
 }
