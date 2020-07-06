028b4fbe82c45a905cb127a3ca5595aa98954cdd
pkunjam <punit17100@iiitnr.edu.in>
2020-07-06 23:35:15 +0530

    Challenge: Are you alive?

---
+++ C# Survival Guide/Assets/Scripts/Damage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+    public int health = 100;
+    public bool active = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) && active)
+        {
+            loss();
+
+            if (health < 1)
+            {
+                Debug.Log("The player has died!");
+                active = false;
+            }
+        }
+	}
+
+    void loss()
+    {
+        health -= Random.Range(0, 100);
+        Debug.Log("Health: " + health);
+    }
+}
