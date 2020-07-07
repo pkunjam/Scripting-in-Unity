8e3e74ecd2126482b086c05f71c48e9b803c5da6
pkunjam <punit17100@iiitnr.edu.in>
2020-07-07 23:49:42 +0530

    Get All Players

---
+++ C# Survival Guide/Assets/Scripts/GetAllPlayers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAllPlayers : MonoBehaviour {
+
+    GameObject[] players;
+    
+	void Start () {
+
+        players = GetAll();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    GameObject[] GetAll()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var p in players)
+        {
+            p.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+        }
+
+        return players;
+    }
+}
