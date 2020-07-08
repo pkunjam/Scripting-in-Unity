f2dd789cd8cef82a04fa002b2d3fcd056e7f70d4
pkunjam <punit17100@iiitnr.edu.in>
2020-07-08 12:48:00 +0530

    Challenge: Position Matters

---
+++ C# Survival Guide/Assets/Scripts/PositionArray.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionArray : MonoBehaviour {
+
+
+    public Vector3[] position;
+    int idx;
+
+	void Start ()
+    {
+        idx = RandomIdx();
+        Debug.Log("Index: " + idx);
+
+        transform.position = setPos(idx);
+	}
+	
+    Vector3 setPos(int i)
+    {
+        return position[i];
+    }
+
+    int RandomIdx()
+    {
+        return Random.Range(0,4);
+    }
+
+}
