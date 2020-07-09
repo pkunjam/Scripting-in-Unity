d369d0346ec3d8a4f9e3ffc6e902f90bcb48e0eb
pkunjam <punit17100@iiitnr.edu.in>
2020-07-09 13:13:58 +0530

    Custom Classes

---
+++ C# Survival Guide/Assets/Scripts/CubeProperties.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties
+{
+    Color col;
+    Vector3 pos;
+
+    public Properties(Color col, Vector3 pos)
+    {
+        this.col = col;
+        this.pos = pos;
+    }
+}
+
+
+public class CubeProperties : MonoBehaviour {
+
+    public Properties stats;
+
+    void Start () {
+
+        stats = new Properties(Color.red, new Vector3(1, 0, 1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
