5a4a575932071bc3497a5f4a3e0859d5ee1dbe0f
pkunjam <punit17100@iiitnr.edu.in>
2020-07-11 13:51:28 +0530

    Challenge: Customer Database

---
+++ C# Survival Guide/Assets/Scripts/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour {
+
+    public CustomerDatabase[] customer;
+    public CustomerDatabase details;
+
+	void Start () {
+
+        details = new CustomerDatabase("John", "Doe", 27, "Male", "Engineer");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ C# Survival Guide/Assets/Scripts/CustomerDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class CustomerDatabase {
+
+    public string firstName;
+    public string lastName;
+    public int age;
+    public string gender;
+    public string occupation;
+
+    public CustomerDatabase(string firstName, string lastName, int age, string gender, string occupation)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+    }
+}
