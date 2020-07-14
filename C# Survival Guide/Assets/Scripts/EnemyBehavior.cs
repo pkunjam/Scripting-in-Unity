cbaf85fa700551557573042c87ccdbc18ad0afbe
pkunjam <punit17100@iiitnr.edu.in>
2020-07-14 19:57:07 +0530

    Working with static types

---
+++ C# Survival Guide/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        SpawnManager.activeEnemies++;
+        Die();
+    }
+
+    private void OnDisable()
+    {
+        SpawnManager.activeEnemies--;
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject, Random.Range(2, 6));
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public static int activeEnemies;
+
+    private void Update()
+    {
+        Debug.Log("Active Enemies: " + activeEnemies);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(enemyPrefab);
+        }
+    }
+
+}
