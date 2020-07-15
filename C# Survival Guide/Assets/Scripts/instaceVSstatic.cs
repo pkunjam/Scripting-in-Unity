d523dcc702732a1430e5c7277170126d61e4a0e9
pkunjam <punit17100@iiitnr.edu.in>
2020-07-15 12:27:00 +0530

    Instance vs Static members

---
+++ C# Survival Guide/Assets/Scripts/instaceVSstatic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public int id;
+    public string name;
+    public static int connectionCount;
+
+    public Player()
+    {
+        connectionCount++;
+    }
+}
+
+public class instaceVSstatic : MonoBehaviour {
+
+	void Start () {
+
+        Player p1 = new Player();
+        Player p2 = new Player();
+
+        Debug.Log(Player.connectionCount);
+	}
+}
