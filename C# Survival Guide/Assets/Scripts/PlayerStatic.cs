71dbd43bb5e3336cb4398fcaade94fc1e73c23d7
pkunjam <punit17100@iiitnr.edu.in>
2020-07-19 19:20:25 +0530

    Utility Helper

---
+++ C# Survival Guide/Assets/Scripts/PlayerStatic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatic : MonoBehaviour {
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UtilityHelper.CreateObject();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            UtilityHelper.SetPositionToZero(this.gameObject);
+        }
+	}
+}
---
+++ C# Survival Guide/Assets/Scripts/UtilityHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilityHelper
+{
+
+    public static void CreateObject()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Capsule);
+    }
+
+    public static void SetPositionToZero(GameObject go)
+    {
+        go.transform.position = Vector3.zero;
+    }
+}
