95bc33253efacb2c1db2598ce9b545d2539bbcfc
pkunjam <punit17100@iiitnr.edu.in>
2020-07-19 12:03:11 +0530

    Properties

---
+++ C# Survival Guide/Assets/Scripts/Properties_GetSet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties_GetSet : MonoBehaviour {
+
+    bool isgameOver;
+
+    public bool IsGameOver
+    {
+        get
+        {
+            return isgameOver;
+        }
+
+        set
+        {
+            if (value == true)
+            {
+                Debug.Log("Game Over!");
+            }
+
+            value = isgameOver;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        isgameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IsGameOver = true;
+        }
+	}
+}
