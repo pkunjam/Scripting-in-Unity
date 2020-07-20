7e43bb3646f2c532a4aa314ccc29a7fc0903ffac
pkunjam <punit17100@iiitnr.edu.in>
2020-07-20 23:21:05 +0530

    Challenge: You have been named

---
+++ C# Survival Guide/Assets/Scripts/ListsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListsManager : MonoBehaviour {
+
+    public List<string> Names;
+    int randomName;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Print();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            randomName = Random.Range(0,Names.Count);
+            Names.RemoveAt(randomName);
+            Print();
+        }
+	}
+
+    void Print()
+    {
+        foreach (var n in Names)
+        {
+            Debug.Log(n);
+        }
+    }
+}
