6a11bbc2bfeea1fcbb18d06ec6b60d4e461c9084
pkunjam <punit17100@iiitnr.edu.in>
2020-07-20 20:43:56 +0530

    Challenge: Declaring Properties

---
+++ C# Survival Guide/Assets/Scripts/PropertiesChallenge.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertiesChallenge : MonoBehaviour {
+
+    float speed;
+    public string c_name;
+
+    // public float Speed {get; private set;}
+
+    // public string Name {get; set;}
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        private set
+        {
+            speed = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(Name);
+        Debug.Log(Speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
