a353f5a781858b2ba895fd589f88f5857d937e3b
pkunjam <punit17100@iiitnr.edu.in>
2020-07-22 00:29:04 +0530

    Enum

---
+++ C# Survival Guide/Assets/Scripts/Enum/ItemDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+
+    public List<ItemEnum> itemDB = new List<ItemEnum>();
+
+	void Start ()
+    {
+        itemDB[2].Observation();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ C# Survival Guide/Assets/Scripts/Enum/ItemEnum.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemEnum
+{
+    public int itemID;
+    public string name;
+
+    public enum ItemType
+    {
+        None,
+        Weapon,
+        Collective,
+        Consumable
+    }
+
+    public ItemType itemType;
+
+    public void Observation()
+    {
+        switch (itemType)
+        {
+            case ItemType.Weapon:
+                Debug.Log("Weapon");
+                break;
+            case ItemType.Consumable:
+                Debug.Log("Consumable");
+                break;
+            case ItemType.Collective:
+                Debug.Log("Collective");
+                break;
+        }
+    }
+}
+
