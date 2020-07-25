8f4c008711a77248538280a4e816a67a66412f13
pkunjam <punit17100@iiitnr.edu.in>
2020-07-25 18:25:05 +0530

    Interfaces

---
+++ C# Survival Guide/Assets/Scripts/Interfaces/Interface_Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface_Enemy : MonoBehaviour, IDamageable
+{
+    public int Health
+    {
+        get
+        {
+            throw new System.NotImplementedException();
+        }
+
+        set
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public void Damage(int damageAmount)
+    {
+        Health -= damageAmount;
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Interfaces/Interface_Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface_Player : MonoBehaviour, IDamageable, IShoot
+{
+	public int Health
+    {
+        get
+        {
+            throw new System.NotImplementedException();
+        }
+
+        set
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public void Damage(int damageAmount)
+    {
+        Health -= damageAmount;
+    }
+
+    public void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+}
