0cfc7f4209782799c0274866ba1fec38d00cb2c7
pkunjam <punit17100@iiitnr.edu.in>
2020-07-26 14:06:39 +0530

    Practical implementation of Delegates and Events

---
+++ C# Survival Guide/Assets/Scripts/Delegates and Events/SphereControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        EventsIntro.onClick += Fall;
+	}
+	
+	void Fall ()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+	}
+}
