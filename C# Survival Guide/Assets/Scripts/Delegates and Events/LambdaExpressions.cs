63083562955b15c4f018802abf8218633ec53962
pkunjam <punit17100@iiitnr.edu.in>
2020-07-27 21:02:55 +0530

    Lambda Expressions

---
+++ C# Survival Guide/Assets/Scripts/Delegates and Events/LambdaExpressions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LambdaExpressions : MonoBehaviour {
+
+    public Func<string, int> CharacterLength;
+
+	void Start ()
+    {
+        CharacterLength = (name) => name.Length;
+        int count = CharacterLength("John");
+
+        Debug.Log("Length: " + count);
+	}
+	
+	//int Length(string str)
+ //   {
+ //       return str.Length;
+	//}
+}
