9f0822f758aae84476d94c118617a2a540d7da39
pkunjam <punit17100@iiitnr.edu.in>
2020-07-29 20:03:36 +0530

    Teleport Events

---
+++ C# Survival Guide/Assets/Scripts/Delegates and Events/EventManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void SetPos(Vector3 pos);
+    public static event SetPos setPos;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (setPos != null)
+            {
+                Vector3 pos = new Vector3(5,2,0);
+                setPos(pos);
+            }
+        }
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Delegates and Events/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        EventManager.setPos += SetPosition;
+	}
+	
+    void SetPosition(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.setPos -= SetPosition;
+    }
+}
