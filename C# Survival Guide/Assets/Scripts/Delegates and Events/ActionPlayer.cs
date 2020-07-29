de651118e59ab0141c656aff610fc32bece7f49b
pkunjam <punit17100@iiitnr.edu.in>
2020-07-29 20:44:10 +0530

    Actions

---
+++ C# Survival Guide/Assets/Scripts/Delegates and Events/ActionPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionPlayer : MonoBehaviour {
+
+    void Start ()
+    {
+        ActionsIntro.onDamageReceived += DamageReceived;
+	}
+
+    void DamageReceived(int health)
+    {
+        Debug.Log("Health: " + health);
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Delegates and Events/ActionsIntro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ActionsIntro : MonoBehaviour {
+
+    public static Action<int> onDamageReceived;
+
+    public int Health {get; set;}
+
+	void Start ()
+    {
+        Health = 100;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Health--;
+
+            if (onDamageReceived != null)
+            {
+                onDamageReceived(Health);
+            }
+        }
+        
+    }
+}
