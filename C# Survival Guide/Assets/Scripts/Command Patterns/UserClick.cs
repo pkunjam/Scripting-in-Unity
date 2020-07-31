23b19603b4b2d3e8b66c1e072808a1d5716100af
pkunjam <punit17100@iiitnr.edu.in>
2020-07-31 23:18:13 +0530

    Command Patterns

---
+++ C# Survival Guide/Assets/Scripts/Command Patterns/UserClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserClick : MonoBehaviour {
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                if (hitInfo.collider.tag == "Cube")
+                {
+                    ICommand click = new ClickCommand(hitInfo.collider.gameObject, new Color(Random.value, Random.value, Random.value));
+                    click.Execute();
+                }
+            }
+        }
+	}
+}
