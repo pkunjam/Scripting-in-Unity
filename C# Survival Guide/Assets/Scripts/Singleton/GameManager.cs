9c93354a6c20ba7bab61052ea07b28b25b154eba
pkunjam <punit17100@iiitnr.edu.in>
2020-07-31 10:06:51 +0530

    Singleton Design Pattern

---
+++ C# Survival Guide/Assets/Scripts/Singleton/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.Log("Null instance");
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void DisplayName()
+    {
+        Debug.Log("This is Game Manager");
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Singleton/SingletonPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonPlayer : MonoBehaviour {
+
+	void Start ()
+    {
+        GameManager.Instance.DisplayName();
+	}
+	
+}
